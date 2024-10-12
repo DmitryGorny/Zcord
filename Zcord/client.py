49e261aa837a903967b3cad7aa544d660f06e6df
Sasha <PP>
2024-10-12 17:21:01 +0300

    Реализация классов + многопоточности

--- Zcord/client.py
+++ Zcord/client.py
@@ -1,36 +1,63 @@
 import socket
 import pyaudio
 
-HOST = "127.0.0.1"  # The server's hostname or IP address
-PORT = 65128  # The port used by the server
 
-FORMAT = pyaudio.paInt16  # Формат звука
-CHANNELS = 1        # Количество каналов (1 для моно)
-RATE = 44100        # Частота дискретизации
+def callback(in_data, frame_count, time_info, status):
+    return (in_data, pyaudio.paContinue)
 
-p = pyaudio.PyAudio()
 
-client = socket.socket()
-hostname = socket.gethostname()  # В документации сказано если использовать в коннект имя хоста то будет непредсказуемость
+def sender():
+    data_to_send = stream_input.read(1024)
+    client.sendall(data_to_send)  # Отправляем данные на сервер
 
-client.connect((HOST, PORT))
 
-stream = p.open(format=FORMAT, channels=CHANNELS,
-                 rate=RATE, input=True,
-                 frames_per_buffer=1024)
+def getter():
+    data_to_read = client.recvfrom(4096)  # Получаем данные с сервера
+    callback(data_to_read, RATE, 5, "on")
 
-print("Начата передача аудио...")
 
-try:
-    while True:
-        data = stream.read(1024)
-        client.sendall(data)  # Отправляем данные на сервер
-except KeyboardInterrupt:
-    print("Передача завершена.")
-finally:
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
-    client.close()
+if __name__ == "__main__":
+    HOST = "127.0.0.1"  # The server's hostname or IP address
+    PORT = 65128  # The port used by the server
 
-client.close()
+    FORMAT = pyaudio.paInt16  # Формат звука
+    CHANNELS = 1        # Количество каналов (1 для моно)
+    RATE = 44100        # Частота дискретизации
+
+    p = pyaudio.PyAudio()
+
+    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    hostname = socket.gethostname()  # В документации сказано если использовать в коннект имя хоста то будет непредсказуемость
+
+    client.connect((HOST, PORT))
+
+    stream_input = p.open(format=FORMAT,
+                          channels=CHANNELS,
+                          rate=RATE,
+                          input=True,
+                          frames_per_buffer=1024)
+
+    stream_output = p.open(format=FORMAT,
+                           channels=CHANNELS,
+                           rate=RATE,
+                           input=True,
+                           output=True,
+                           stream_callback=callback)
+
+    print("Начата передача аудио..., для завершения ctrl + c")
+    sender()
+    try:
+        while True:
+            sender()
+            getter()
+
+    except KeyboardInterrupt:
+        print("Передача завершена.")
+    finally:
+        stream_input.stop_stream()
+        stream_input.close()
+        stream_output.stop_stream()
+        stream_output.close()
+        p.terminate()
+        client.close()
+
--- Zcord/server.py
+++ Zcord/server.py
@@ -1,55 +1,61 @@
 import socket
 import pyaudio
 import wave
-import sys
-import time
+import asyncio
+import threading
 
 
 def callback(in_data, frame_count, time_info, status):
     return (in_data, pyaudio.paContinue)
 
 
-HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 65128  # Port to listen on (non-privileged ports are > 1023)
+class Client(object):
+    def __init__(self, address):
+        self.address = address
 
-p = pyaudio.PyAudio()
-FORMAT = pyaudio.paInt16
-CHANNELS = 1
-RATE = 44100
-
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-server.bind((HOST, PORT))
-server.listen()
-print("Server starts")
-
-conn, addr = server.accept()
-
-print("connection: ", conn)
-print("client address: ", addr)
+    def return_address(self):
+        return self.address
 
 
-with wave.open("output.wav", "wb") as f:  # Открываем файл для записи в бинарном режиме
-    f.setnchannels(CHANNELS)
-    f.setsampwidth(2)  # 2 байта для paInt16
-    f.setframerate(RATE)
+class Server(object):
+    def __init__(self):
+        self.HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+        self.PORT = 65128  # Port to listen on (non-privileged ports are > 1023)
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        print("Server starts")
 
-    stream = p.open(format=FORMAT,
-                    channels=CHANNELS,
-                    rate=RATE,
-                    input=True,
-                    output=True,
-                    stream_callback=callback)
+        #self.f = wave.open("output.wav", "wb")  # Открываем файл для записи в бинарном режиме
 
-    while True:
-        data = conn.recv(1024)
-        if not data:
-            break
-        callback(data, RATE, 5, "on")
-        f.writeframes(data)
-    print("Получены данные")
+    def read_request(self):
+        self.server.bind((self.HOST, self.PORT))
+        self.data, self.address = self.server.recvfrom(4096)
+        print(f"Connected to: {self.address}")
+        Client(address=self.address)
 
-conn.close()
+        #self.f.setnchannels(CHANNELS)
+        #self.f.setsampwidth(2)  # 2 байта для paInt16
+        #self.f.setframerate(RATE)
 
-print("Server ends")
-server.close()
+        while True:
+            self.data, self.address = self.server.recvfrom(4096)
+            if not self.data:
+                break
+
+            #self.f.writeframes(data)
+
+            client_thread = threading.Thread(target=self.send_request(), args=(self.data, self.address, self.server))
+            client_thread.start()
+            #self.send_request()
+
+    def send_request(self):
+        self.server.sendto(self.data, self.address)
+
+    def close_server(self):
+        print("Server ends")
+        self.f.close()
+        self.server.close()
+
+
+if __name__ == "__main__":
+    server_obj = Server()
+    server_obj.read_request()

db20096e11c616700b2534da25cbfa98816f27a8
Sasha <PP>
2024-10-16 21:30:32 +0300

    Добавление даты времени и активности чата

--- Zcord/logic/Message/message_client.py
+++ Zcord/logic/Message/message_client.py
@@ -2,14 +2,32 @@ import socket
 import threading
 
 
-class MessageConnection(object):
+class MainInterface:
+    __current_chat = 1
+
+    def __init__(self):
+        pass
+
+    def change_chat(self, current_chat):
+        MainInterface.__current_chat = current_chat
+
+    def return_current_chat(self):
+        return MainInterface.__current_chat
+
+
+class MessageConnection(MainInterface):
     def __init__(self, client_tcp):
         self.client_tcp = client_tcp
+        super().__init__()
 
     def send_message(self):
         while True:
-            msg = f"{nickname}: {input()}"
-            self.client_tcp.sendall(msg.encode("utf-8"))
+            a = input()
+            if a == "change chat":
+                self.change_chat(input("Введите id чата: "))
+            else:
+                msg = f"{self.return_current_chat()}, {nickname}, {a}".encode("utf-8")
+                self.client_tcp.sendall(msg)
 
     def recv_message(self):
         while True:
@@ -26,15 +44,21 @@ class MessageConnection(object):
 
 
 if __name__ == "__main__":
-    SERVER_IP = "26.124.194.150"  # IP адрес сервера
+    SERVER_IP = "26.36.124.241"  # IP адрес сервера
     SERVER_PORT = 55555  # Порт, используемый сервером
 
     nickname = input("Введите свой ник: ")
 
-    client_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_tcp.connect((SERVER_IP, SERVER_PORT))
+    all_chats = [1, 2]
 
-    message_binder = MessageConnection(client_tcp)
+    try:
+        client_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_tcp.connect((SERVER_IP, SERVER_PORT))
+
+        message_binder = MessageConnection(client_tcp)
+    except ConnectionRefusedError:
+        print("Не удалось подключится к серверу или сервер неактивен")
+        exit(0)
 
     print("Старт клиента сообщений")
     try:
@@ -43,5 +67,10 @@ if __name__ == "__main__":
 
         write_thread = threading.Thread(target=message_binder.send_message)
         write_thread.start()
+
+        while True:
+            pass
+
     except KeyboardInterrupt:
         print("Прекращение работы клиента сообщений")
+        exit(0)
--- Zcord/logic/Message/message_server.py
+++ Zcord/logic/Message/message_server.py
@@ -1,31 +1,67 @@
 import socket
 import threading
+from datetime import datetime
+import json
 
 
-class MessageServer(object):
+class MessageRoom(object):
     def __init__(self, server_msg):
         self.server_msg = server_msg
+        self.cache_chat = {"chat_id": {
+            1: [],
+            2: []
+            }
+        }
+        self.nicknames_in_chats = {
+            1: [],
+            2: []
+        }
+        self.f = open('cache_chat.json', 'w')
 
-    def broadcast(self, message):
-        for client in clients:
-            client.send(message)
+    def broadcast(self, msg):
+        chat_code = msg[0]
+        nickname = msg[1]
+        message = msg[2]
+        for client in self.nicknames_in_chats[chat_code]:
+            clients[client].send(f"{nickname}: {message}".encode('utf-8'))
 
-    def handle(self, client):
+    def handle(self, client, nickname):
         while True:
             try:
                 # Broadcasting Messages
-                message = client.recv(1024)
-                self.broadcast(message)
+                msg = client.recv(1024)
+                msg = msg.decode('utf-8').split(", ")
+                chat_code = int(msg[0])
+                nickname = msg[1]
+                message = msg[2]
+                if nickname not in self.nicknames_in_chats[chat_code]:  # ВОТ ЗДЕСЬ
+                    self.nicknames_in_chats[chat_code].append(nickname)
+                else:
+                    pass
+                date_now = f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] "
+                date_now = date_now
+                self.broadcast((chat_code, date_now + nickname, message))
+
+                self.cache_chat["chat_id"][chat_code].append(message)
+
+                if len(self.cache_chat["chat_id"][chat_code]) >= 20:
+                    del self.cache_chat["chat_id"][chat_code][0]
+
             except ConnectionResetError:
                 # Removing And Closing Clients
-                index = clients.index(client)
-                clients.remove(client)
+                for j in self.nicknames_in_chats:
+                    if nickname in self.nicknames_in_chats[j]:
+                        self.nicknames_in_chats[j].remove(nickname)
+                        self.broadcast((j, nickname, f"Пользователь {nickname} вышел!"))
+                clients.pop(nickname)
                 client.close()
-                nickname = nicknames[index]
-                self.broadcast(f"Пользователь {nickname} вышел!".encode('utf-8'))
+                #self.broadcast(f"Пользователь {nickname} вышел!".encode('utf-8'))
                 print(f"{nickname} left!")
-                nicknames.remove(nickname)
-                break
+                #if len(nicknames) == 0:
+                    #for i in self.cache_chat:
+                      #  json.dump(self.cache_chat, self.f)
+                   # self.f.close()
+                #break
 
     def receive(self):
         while True:
@@ -36,29 +72,27 @@ class MessageServer(object):
             # Request And Store Nickname
             client.send('NICK'.encode('utf-8'))
             nickname = client.recv(1024).decode('utf-8')
-            nicknames.append(nickname)
-            clients.append(client)
+            clients[nickname] = client
 
             # Print And Broadcast Nickname
             print(f"Nickname is {nickname}")
-            msg_obj.broadcast(f"Пользователь {nickname} подключился!".encode('utf-8'))
+            #self.broadcast(f"Пользователь {nickname} подключился!".encode('utf-8'))
             client.send('Подключено к серверу'.encode('utf-8'))
 
             # Start Handling Thread For Client
-            thread = threading.Thread(target=msg_obj.handle, args=(client,))
+            thread = threading.Thread(target=msg_obj.handle, args=(client, nickname,))
             thread.start()
 
 
 if __name__ == "__main__":
-    HOST = "26.124.194.150"
+    HOST = "26.36.124.241"
     PORT = 55555
     server_msg = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_msg.bind((HOST, PORT))
     server_msg.listen()
 
-    clients = []
-    nicknames = []
+    clients = {}
 
-    msg_obj = MessageServer(server_msg)
+    msg_obj = MessageRoom(server_msg)
 
     msg_obj.receive()

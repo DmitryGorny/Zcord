e50ae61e04a2d9ffda2e52b36b7f11850ecde17e
DisitaZX <DisitaZX@yandex.ru>
2025-07-29 02:51:21 +0500

    Фикс get api

--- API/ZcordDB/serializers/FriendsAddingSerializer.py
+++ API/ZcordDB/serializers/FriendsAddingSerializer.py
@@ -1,12 +1,8 @@
 from rest_framework import serializers
 from ..models import FriendsAdding
-from .UserSerializer import UserSerializer
 
 
 class FriendsAddingSerializer(serializers.ModelSerializer):
-    sender = UserSerializer(read_only=True)
-    friend = UserSerializer(read_only=True)
-
     class Meta:
         model = FriendsAdding
-        fields = ['id', 'sender', 'friend', 'date']
+        fields = '__all__'
--- API/ZcordDB/serializers/MessageSerializer.py
+++ API/ZcordDB/serializers/MessageSerializer.py
@@ -1,12 +1,8 @@
 from rest_framework import serializers
 from ..models import Message
-from .UserSerializer import UserSerializer
 
 
 class MessageSerializer(serializers.ModelSerializer):
-    sender = UserSerializer(read_only=True)
-    was_seen_display = serializers.CharField(source='get_was_seen_display', read_only=True)
-
     class Meta:
         model = Message
-        fields = ['id', 'chat_id', 'sender', 'message', 'created_at', 'updated_at', 'was_seen', 'was_seen_display']
+        fields = '__all__'
